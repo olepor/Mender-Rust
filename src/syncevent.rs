33d4e3f659a7df11fe6fe3ee113cc5a111897d89
Ole Petter <olepor@matnat.uio.no>
2019-08-19 19:58:54 +0200

    Simplify the Event producer logic in syncevent
    Signed-off-by: Ole Petter <olepor@matnat.uio.no>

--- src/syncevent.rs
+++ src/syncevent.rs
@@ -7,46 +7,9 @@ use std::time; // Multiple producer, single consumer channel.
 
 // These are based on the config file parameters found in:
 // /etc/mender/mender.conf
-enum Event {
+enum Events {
     InventoryUpdate,
     CheckForUpdate,
-}
-
-struct InventoryUpdate {
-    interval: time::Duration,
-    channel: mpsc::Sender<Event>,
-}
-
-impl InventoryUpdate {
-    fn new(interval: time::Duration, channel: mpsc::Sender<Event>) -> InventoryUpdate {
-        InventoryUpdate { interval, channel }
-    }
-    fn produce(&self) -> bool {
-        loop {
-            thread::sleep(self.interval);
-            self.channel
-                .send(Event::InventoryUpdate) // Time::now ?
-                .expect("UpdateCheck: Failed to send signal on the channel"); // TODO -- What to send here?
-        }
-    }
-}
-
-struct UpdateCheck {
-    interval: time::Duration,
-    channel: mpsc::Sender<Event>,
-}
-
-impl UpdateCheck {
-    fn new(interval: time::Duration, channel: mpsc::Sender<Event>) -> UpdateCheck {
-        UpdateCheck { interval, channel }
-    }
-    fn produce(&self) -> bool {
-        loop {
-            self.channel
-                .send(Event::CheckForUpdate)
-                .expect("UpdateCheck: Failed to send signal on the channel"); // TODO -- What to send here?
-        }
-    }
 }
 
 use std::fs::File;
@@ -71,29 +34,29 @@ impl Default for IntervalConf {
 
 // TODO -- Maybe add custom deserialization to the IntervalConf, so that
 // it can be embedded as a struct to Evnt(?).
-struct Evnt {
+struct Event {
     inventory_check_interval: time::Duration,
     update_check_interval: time::Duration,
-    publisher: mpsc::Sender<Event>,
-    pub events: mpsc::Receiver<Event>,
+    publisher: mpsc::Sender<Events>,
+    pub events: mpsc::Receiver<Events>,
 }
 
-impl Evnt {
+impl Event {
     // Initialize the Evnt struct with an InventoryCheck at once,
     // and then an update check after a minute.
-    pub fn new() -> Box<Evnt> {
+    pub fn new() -> Event {
         let file = File::open("./dummies/mender.conf").expect("Error opening file");
         let reader = BufReader::new(file);
         let conf: IntervalConf =
             serde_json::from_reader(reader).expect("Failed to parse the config file");
 
         let (tx1, rx) = mpsc::channel();
-        Box::new(Evnt {
+        Event {
             publisher: tx1,
             events: rx,
             inventory_check_interval: time::Duration::from_secs(conf.inventory_check_interval),
             update_check_interval: time::Duration::from_secs(conf.update_check_interval),
-        })
+        }
     }
     // Run the event Creator loop
     pub fn run(&self) {
@@ -102,14 +65,18 @@ impl Evnt {
         let tx1 = mpsc::Sender::clone(&self.publisher);
         let update_interval = self.update_check_interval;
         thread::spawn(move || {
-            let uc = UpdateCheck::new(update_interval, tx1);
-            uc.produce();
+            loop {
+                thread::sleep(update_interval);
+                tx1.send(Events::CheckForUpdate).unwrap();
+            }
         });
         let tx2 = mpsc::Sender::clone(&self.publisher);
         let inventory_interval = self.inventory_check_interval;
         thread::spawn(move || {
-            let iu = InventoryUpdate::new(inventory_interval, tx2);
-            iu.produce();
+            loop {
+                thread::sleep(inventory_interval);
+                tx2.send(Events::InventoryUpdate).unwrap();
+            }
         });
     }
 }
